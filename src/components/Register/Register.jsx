94d03db95e669b289859d0e46c991ea19bf7b130
RakibulIslam101 <rakibhassan.jes@gmail.com>
2023-10-03 04:01:44 +0600

    first commit

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,28 @@
+
+
+const Register = () => {
+
+    const handleRegister = e =>{
+        e.preventDefault();
+      console.log('form submit');
+      const email = e.target.Email.value; 
+      const password = e.target.Password.value;
+      console.log(email,password);
+    }
+    return (
+        <div className="">
+            <div className="mx-auto md:w-1/2">
+            <h2 className="text-3xl mb-8">Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <input className="bg-red-200 mb-4 w-3/4 py-2 px-4" type="email" name="Email"placeholder="Email Address" id="" />
+                <br/>
+                <input className="bg-red-200 mb-4 w-3/4 py-2 px-4" type="password" name="Password" placeholder="Password" id="" />
+                <br/>
+                <input className=" mb-4 w-3/4 btn btn-secondary" type="submit" value="Register" />
+            </form>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
